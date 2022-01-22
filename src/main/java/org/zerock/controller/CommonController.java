fc16f81912a21010b7a29ad96e8ead8226ac725f
jiseong0612 <hth5545@gmail.com>
2022-01-22 16:56:50 +0900

    권한 접근, 에러, 강제이동 구현

---
+++ src/main/java/org/zerock/controller/CommonController.java
@@ -0,0 +1,56 @@
+package org.zerock.controller;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class CommonController {
+	
+	/**
+	 * 로그인 실패시
+	 * 
+	 * @param auth
+	 * @param model
+	 */
+	@GetMapping("/accessError")
+	public void accessError(Authentication auth, Model model) {
+		System.out.println("access Denied : " + auth);
+		model.addAttribute("msg", "Access Demied");
+	}
+
+	/**
+	 * 로그인
+	 * 
+	 * @param error
+	 * @param logout
+	 * @param model
+	 */
+	@GetMapping("/customLogin")
+	public void customLogin(String error, String logout, Model model) {
+		System.out.println("로그인을 하던 안 하던 우선 여기로 온다");
+		log.info("error : " + error);
+		log.info("logout : " + logout);
+
+		if (error != null) {
+			model.addAttribute("error", "Login Error Check your Account");
+		}
+
+		if (logout != null) {
+			model.addAttribute("logout", "LogOut!!!");
+		}
+	}
+
+	/**
+	 * 로그아웃
+	 */
+	@GetMapping("/customLogout")
+	public void customLogout() {
+
+	}
+
+}
---
+++ src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,45 @@
+package org.zerock.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import lombok.extern.log4j.Log4j;
+
+//AuthenticationSuccessHandler : 로그인 성공 이후에 특정한 동작을 하도록 제어
+@Log4j
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth)
+			throws IOException, ServletException {
+		log.info("login success...");
+		List<String> roleNames = new ArrayList<String>();
+		
+		auth.getAuthorities().forEach(authority -> {
+			roleNames.add(authority.getAuthority());
+		});
+		
+		log.warn("Role Names : "+ roleNames);
+		
+		if(roleNames.contains("ROLE_ADMIN")) {
+			response.sendRedirect("/sample/admin");
+			return;
+		}
+		
+		if(roleNames.contains("ROLE_MANAGER")) {
+			response.sendRedirect("/sample/member");
+			return;
+		}
+		
+		response.sendRedirect("/");
+	}
+
+}
